6d21b884e9657886f760dc3c97fd394cfefa1565
simon <yungsimonprod@gmail.com>
2026-06-13 10:33:50 +0200

    Add explicit create/update/delete access to Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -8,6 +8,9 @@ const Media: CollectionConfig = {
   },
   access: {
     read: () => true,
+    create: ({ req: { user } }) => Boolean(user),
+    update: ({ req: { user } }) => Boolean(user),
+    delete: ({ req: { user } }) => Boolean(user),
   },
   fields: [
     {
